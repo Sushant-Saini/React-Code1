4bb9fe8b92cd197251d92f475e75e743467e25c7
Sushant-Saini <59145085+Sushant-Saini@users.noreply.github.com>
2020-10-19 15:32:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Footer from './Footer'
+import TodoItem from './Todoitem'
+
+function App() {
+  return (
+    <div className="App">
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
